3e8db8cd6302a7e86b9cdebae7381583137bc0e6
Evgeny Sikachev <esikachev@mirantis.com>
2015-05-19 00:01:06 +0300

    Fix typpo

--- shellper/tests/integration/test_create_delete_event.py
+++ shellper/tests/integration/test_create_delete_event.py
@@ -9,7 +9,7 @@ class TestCreateListDeleteEvent(object):
             'date': '18.05.2015',
             'summary': 'How clone git repository?'
         }]
-        self.ids_of_evens = []
+        self.ids_of_events = []
         self.calendar = base.Base()
         self.create_event(config)
         self.delete_event()
@@ -17,12 +17,12 @@ class TestCreateListDeleteEvent(object):
     def create_event(self, config):
         run.add_links(self.calendar, config)
         for event in config:
-            self.ids_of_evens.append(
+            self.ids_of_events.append(
                 self.calendar.create_event(event))
 
     def list_events(self):
         self.calendar.get_event_list()
 
     def delete_event(self):
-        for id_of_event in self.ids_of_evens:
+        for id_of_event in self.ids_of_events:
             self.calendar.delete_event(id_of_event)
