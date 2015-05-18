ea364337418b7ca4e1161ab8d71877a575416a15
Evgeny Sikachev <esikachev@mirantis.com>
2015-05-18 21:46:36 +0300

    Fix coverage

--- shellper/tests/unit/test_main.py
+++ shellper/tests/unit/test_main.py
@@ -1,6 +1,7 @@
 import mock
 import testtools
 
+import shellper.base as main_base
 import shellper.main as main_function
 import shellper.tests.base as base
 
@@ -11,3 +12,11 @@ class TestMain(testtools.TestCase):
     @mock.patch('shellper.validation.validate')
     def test_main(self, mock_validate, mock_argparse):
         main_function.main()
+
+    def test_add_links(self):
+        config = [{
+            'time': '22:00',
+            'date': '18.05.2015',
+            'summary': 'How clone git repository?'
+        }]
+        main_function.add_links(main_base.Base(), config)
