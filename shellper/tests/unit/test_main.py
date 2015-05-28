a0e979b1a65b794e45c0f6c34c43992bf3424479
Evgeny Sikachev <esikachev@mirantis.com>
2015-05-28 16:51:40 +0300

    Improvement coverage of unit-tests
    Add coverage of 2 methods from main.py
    close #55
    
    Change-Id: I330e704cd2f8f517f846644a70ca577e512e03c7

--- shellper/tests/unit/test_main.py
+++ shellper/tests/unit/test_main.py
@@ -2,20 +2,26 @@ import mock
 import testtools
 
 import shellper.base as main_base
-import shellper.main as main_function
-import shellper.tests.fake_class as base
+import shellper.main as main
 
 
 class TestMain(testtools.TestCase):
+    @mock.patch('shellper.main.open_file')
+    @mock.patch('shellper.main.parsing_args')
     @mock.patch('shellper.base.Base.create_event', return_value=None)
     @mock.patch('shellper.base.Base.search_query', return_value=None)
     @mock.patch('shellper.base.Base.get_event_list', return_value=None)
-    @mock.patch('argparse.ArgumentParser.parse_args',
-                return_value=base.FakeClass())
     @mock.patch('shellper.validation.validate')
-    def test_main(self, mock_validate, mock_argparse, mock_getlist,
-                  mock_query, mock_create):
-        main_function.main()
+    def test_main(self, mock_validate, mock_getlist, mock_query, mock_create,
+                  mock_argparse, mock_openfile):
+        main.main()
+
+    @mock.patch('argparse.ArgumentParser.parse_args')
+    def test_parsing_args(self, mock_argparse):
+        main.parsing_args()
+
+    def test_open_file(self):
+        main.open_file('etc/template.yaml')
 
     @mock.patch('shellper.base.Base.search_query', return_value=None)
     def test_add_links(self, mock_query):
@@ -24,4 +30,4 @@ class TestMain(testtools.TestCase):
             'date': '18.05.2015',
             'summary': 'How clone git repository?'
         }]
-        main_function.add_links(main_base.Base(), config)
+        main.add_links(main_base.Base(), config)
