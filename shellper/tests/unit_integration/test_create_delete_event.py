14449ba745fe7994720e9dbcc61f3e1db21efce2
kozlovpn <kozlov.pavel.nikolaich@gmail.com>
2015-05-28 16:31:24 +0300

    Add unit tests for integration tests
    Write unit tests for integration tests. Add command
    for run integration unit tests
    
    Change-Id: I3fb2ce2cb2df46b2e73fa4579eca890ec6603bd2

---
+++ shellper/tests/unit_integration/test_create_delete_event.py
@@ -0,0 +1,44 @@
+import mock
+import testtools
+
+import shellper.tests.integration.test_create_delete_event as test_integration
+
+
+class TestCreateDeleteEvent(testtools.TestCase):
+    def setUp(self):
+        super(TestCreateDeleteEvent, self).setUp()
+        self.test_cld = test_integration.TestCreateListDeleteEvent()
+
+    @mock.patch('shellper.tests.integration.test_create_delete_event.'
+                'TestCreateListDeleteEvent.delete_event')
+    @mock.patch('shellper.tests.integration.test_create_delete_event.'
+                'TestCreateListDeleteEvent.list_events')
+    @mock.patch('shellper.tests.integration.test_create_delete_event.'
+                'TestCreateListDeleteEvent.create_event')
+    @mock.patch('shellper.tests.integration.test_create_delete_event.'
+                'TestCreateListDeleteEvent.prepare_config')
+    def test_scenario(self, mock_prepare, mock_create, mock_list, mock_delete):
+        self.test_cld.scenario()
+
+    @mock.patch('shellper.main.open_file')
+    @mock.patch('shellper.main.parsing_args')
+    def test_prepare_config(self, mock_args, mock_open):
+        self.test_cld.prepare_config()
+
+    @mock.patch('shellper.base.Base.create_event')
+    @mock.patch('shellper.main.add_links')
+    def test_create_event(self, mock_links, mock_create_event):
+        config = {
+            'time': '18:00',
+            'date': '03.05',
+            'summary': 'How clone git repository?'
+        }
+        test_integration.TestCreateListDeleteEvent().create_event(config)
+
+    @mock.patch('shellper.base.Base.get_event_list')
+    def test_list_events(self, mock_get_event_list):
+        self.test_cld.list_events()
+
+    @mock.patch('shellper.base.Base.delete_event')
+    def test_delete_event(self, mock_delete_event):
+        self.test_cld.delete_event()
