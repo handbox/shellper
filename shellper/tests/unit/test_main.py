66924bf785e441f352966c19043ea34322588fc4
Evgeny Sikachev <esikachev@mirantis.com>
2015-06-19 17:03:45 +0300

    Fix coverage
    Coverage is 100%
    closes-bug: 1466875
    
    Change-Id: Icb211c51b5ad2dc4fe32e4f44913a5d84d4fc116

--- shellper/tests/unit/test_main.py
+++ shellper/tests/unit/test_main.py
@@ -9,12 +9,23 @@ import shellper.tests.unit.fake_class as base
 class TestMain(testtools.TestCase):
     @mock.patch('shellper.base.Base.create_event', return_value=None)
     @mock.patch('shellper.base.Base.search_query', return_value=None)
-    @mock.patch('shellper.base.Base.get_event_list', return_value=None)
+    @mock.patch('shellper.base.Base.get_event_list', return_value=['event1',
+                                                                   'event2'])
     @mock.patch('argparse.ArgumentParser.parse_args',
                 return_value=base.FakeClass())
     @mock.patch('shellper.validation.validate')
-    def test_main(self, mock_validate, mock_argparse, mock_getlist,
-                  mock_query, mock_create):
+    def test_main_with_events(self, mock_validate, mock_argparse, mock_getlist,
+                              mock_query, mock_create):
+        main_function.main()
+
+    @mock.patch('shellper.base.Base.create_event', return_value=None)
+    @mock.patch('shellper.base.Base.search_query', return_value=None)
+    @mock.patch('shellper.base.Base.get_event_list', return_value=[])
+    @mock.patch('argparse.ArgumentParser.parse_args',
+                return_value=base.FakeClass())
+    @mock.patch('shellper.validation.validate')
+    def test_main_without_events(self, mock_validate, mock_argparse,
+                                 mock_getlist, mock_query, mock_create):
         main_function.main()
 
     @mock.patch('shellper.base.Base.search_query', return_value=True)
